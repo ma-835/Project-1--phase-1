52072b8c5ae345cc0308b699a6e74b1069e88479
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-11 13:15:56 -0400

    uses a forEach method to loop through the object in db.json file

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 fetch(`http://localhost:3000/cats`)
 .then(response=>response.json())
-.then(cats => cats.forEach(cat => console.log(cat)))
+.then(cats => cats.forEach(cat => rendercat(cat)))
+// uses a forEach method to loop through the object in db.json file 
+
+
+
+
+
  
+
+
