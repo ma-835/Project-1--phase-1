966d5ae7e91d88dc2d05b6b2207e490afed8eb66
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-05-11 17:58:57 -0400

    creates infoContainer variable

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project-1--phase-1
 Final version of project 1 
+Instructions on how to open the website 
+- clone down repo 
+- Follow Thomas instructions on how to finish this up 
+
--- main.js
+++ main.js
@@ -3,10 +3,6 @@ fetch(`http://localhost:3000/cats`)
 .then(cats => cats.forEach(cat => rendercat(cat)))
 // uses a forEach method to loop through the object in db.json file 
 
-
-
-
-
 function renderCat(cat) {
 const container = document.querySelector('.container'); // creates container variable
 const catObject= document.createElement('div') // creates catObject variable
@@ -58,6 +54,11 @@ revealButton.addEventListener("click",function () { // adds eventListener functi
 
 revealButton.textContent = "Reveal answer" // adds a textContent attribute to revealButton variable
 catObject.classList.add("revealButton") // adds revealButton className to catObject variable
+
+
+// infoContainer part 
+const infoContainer = document.createElement("div"); // creates infoContainer variable 
+
 
 
 
